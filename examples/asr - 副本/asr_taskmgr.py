c4f8ddb809c41123fa5f2e4d86c110d80f122c50
STARWORK\oho0o <david@mworld.fun>
2024-03-28 01:23:44 +0800

    可以正常工作的版本version0.0.8

--- examples/asr - 副本/asr_taskmgr.py
+++ examples/asr - 副本/asr_taskmgr.py
@@ -1,7 +1,9 @@
 import sqlite3
 import os
+import time
 import multiprocessing
 import logging
+import json
 import tencentcloud
 import myasr_client
 
@@ -13,11 +15,6 @@ def upload_file(file_name):
 # from key to url
 def key2url(key):
     return f"https://kuai-video-1255989664.cos.ap-guangzhou.myqcloud.com/3xt2waf6hw86e22/www_video_mp3/{key}.mp3"
-
-# Define a function to upload a file to COS
-def upload_file(file_name):
-    # Upload the file to COS
-    
 
 
 # Define a function to upload mp3 to COS
@@ -33,17 +30,22 @@ def upload_mp3(queue_OnDisk, queue_OnCos):
     db_file = "3xt2waf6hw86e22.db"
     conn = sqlite3.connect(db_file)
     cursor = conn.cursor()
-    logger.debug("Uploading mp3 to COS")
+    logger.debug("Uploading mp3 to COS begin")
+    j = 0;
     while True:
         key = queue_OnDisk.get()
         if key is None:
             break
         # Upload the mp3 file to COS
         upload_file(key)
+        j += 1
+        logger.debug(f"Uploaded No.{j}  {key}.mp3 to COS")
         # Update the status in the database
         cursor.execute("UPDATE mp3Item SET status = ? WHERE id = ?", ("OnCos", key))
         conn.commit()
+        queue_OnCos.put(key)
     conn.close()
+    logging.debug("Uploading mp3 to COS end!")
 
 
 # Define a function to send request to ASR
@@ -59,22 +61,33 @@ def send_req(queue_OnCos, queue_AsrIng):
     db_file = "3xt2waf6hw86e22.db"
     conn = sqlite3.connect(db_file)
     cursor = conn.cursor()
-    logger.debug("Sending request to ASR")
+    logger.debug("Send requests to ASR begin!")
     my_create_rec = myasr_client.MyASRClient("ap-guangzhou")
+    i = 0
+    j = 0
     while True:
         key = queue_OnCos.get()
         if key is None:
             break
         # Send request to ASR
+        logger.debug(f"Sending request {key} to ASR")
         request_id, task_id = my_create_rec.create_rec_task(key2url(key))
+        i += 1
+        j += 1
+        if i > 19:
+            time.sleep(1)
+            i = 0
         if request_id is not None:
             # Update the status in the database
+            logger.debug(f"No.{j} request_id: {request_id}, task_id: {task_id}")
             cursor.execute("UPDATE mp3Item SET status = ?, request_id = ?, task_id = ? WHERE id = ?", ("AsrIng", request_id, task_id, key))
             conn.commit()
-            queue_AsrIng.put(key,task_id)
+            queue_AsrIng.put((key,task_id))
         else:
             logging.error(f"Failed to create rec task for {key}")
+            queue_OnCos.put(key)
     conn.close()
+    logging.debug("Send requests to ASR end!")
 
 
 # Define a function to get result from ASR
@@ -90,28 +103,42 @@ def get_result(queue_AsrIng, queue_GetResult):
     db_file = "3xt2waf6hw86e22.db"
     conn = sqlite3.connect(db_file)
     cursor = conn.cursor()
-    logger.debug("Getting result from ASR")
+    logger.debug("Get result from ASR begin!")
     my_describe_task = myasr_client.MyASRClient("ap-guangzhou")
+    i = 0
+    j = 0
     while True:
         if not queue_AsrIng.empty():
             data = queue_AsrIng.get()
             if data is not None:
                 key, task_id = data
+                logger.debug(f"Getting result for {key},{task_id}")
                 # Rest of your code
+                # Get result from ASR
+                success, result = my_describe_task.query_rec_task(task_id)
+                i += 1
+                j += 1
+                if i > 49:
+                    time.sleep(1)
+                    i = 0
+                if success:
+                    # Convert the list to a JSON string
+                    result_str = json.dumps(result)
+                    # Update the status in the database
+                    cursor.execute("UPDATE mp3Item SET status = ?, asr_data = ? WHERE id = ?", ("GetResult", result_str, key))
+                    conn.commit()
+                    logger.debug(f"Got No.{j} result for {key}")
+                    queue_GetResult.put(key)
+                else:
+                    logging.error(f"Failed to get result for {key}")
+                    queue_AsrIng.put(key,task_id)
             else:
                 break
         else:
-            break
-        # Get result from ASR
-        success, result = my_describe_task.query_rec_task(task_id)
-        if success:
-            # Update the status in the database
-            cursor.execute("UPDATE mp3Item SET status = ?, asr_data = ? WHERE id = ?", ("GetResult", result, key))
-            conn.commit()
-            queue_GetResult.put(key)
-        else:
-            logging.error(f"Failed to get result for {key}")
+            time.sleep(1)
+        
     conn.close()
+    logging.debug("Get result from ASR end!")
 
 
 
@@ -146,7 +173,8 @@ if __name__ == '__main__':
 
     # Specify the directory to check
     # directory = "D:\test\3xt2waf6hw86e22\www_video_mp3"
-    directory = "D:\\test\\www_video_mp3"
+    # directory = "D:\\test\\www_video_mp3"
+    directory = "E:\\data\\3xt2waf6hw86e22\\www_video"
 
     # Get all the file names (without extensions) in the directory
     file_names = [os.path.splitext(file)[0] for file in os.listdir(directory)]
@@ -172,18 +200,29 @@ if __name__ == '__main__':
     data = cursor.fetchall()
 
     # Insert each data into the corresponding queue based on its status
+    ondisk_count = 0
+    oncos_count = 0
+    asring_count = 0
+    finish_count = 0
     for row in data:
-        key, status, _, task_id, _ = row
+        key, status, _, task_id, result_str = row
         if status == "OnDisk":
             queue_OnDisk.put(key)
+            ondisk_count += 1
         elif status == "OnCos":
             queue_OnCos.put(key)
+            oncos_count += 1
         elif status == "AsrIng":
             queue_AsrIng.put((key, task_id))
+            asring_count += 1
         elif status == "GetResult":
-            queue_GetResult.put(key)
+            finish_count += 1
         else:
-            logging.error(f"Invalid status: {status}")
+            logger.error(f"Invalid status: {status}")
+    logger.debug(f"ondiskcount: {ondisk_count}，{queue_OnDisk.qsize()}")
+    logger.debug(f"oncoscount: {oncos_count}")
+    logger.debug(f"asringcount: {asring_count}")
+    logger.debug(f"finishcount: {finish_count}")
 
 
 
@@ -200,13 +239,14 @@ if __name__ == '__main__':
 
 
     # Wait for all the queues to be empty
-    while not (queue_OnDisk.empty() and queue_OnCos.empty() and queue_AsrIng.empty() and queue_GetResult.empty() ):
+    while not (queue_OnDisk.empty() and queue_OnCos.empty() and queue_AsrIng.empty() ):
         pass
 
     # Put None into each queue to signal the processes to exit
+    logging.debug("Put None into each queue to signal the processes to exit")
     queue_OnDisk.put(None)
     queue_OnCos.put(None)
-    queue_AsrIng.put(None)
+    queue_AsrIng.put(None)  
     queue_GetResult.put(None)
 
     # Wait for all the processes to finish
--- examples/asr - 副本/myasr_client.py
+++ examples/asr - 副本/myasr_client.py
@@ -22,7 +22,7 @@ class MyASRClient:
         # Configure your Tencent Cloud API credentials
         logger.debug("Initializing MyASRClient")
         self.cred = credential.EnvironmentVariableCredential().get_credential()
-        logger.debug("Creating ASR client,cred: %s",self.cred)
+        logger.debug("Creating ASR client,cred: %s",self.cred.secret_id)
         self.region = region
         self.client = asr_client.AsrClient(self.cred, self.region)
         self.create_rec_task_req = models.CreateRecTaskRequest()
@@ -30,6 +30,7 @@ class MyASRClient:
         params = {"ChannelNum": 1, "ResTextFormat": 2, "SourceType": 0, "ConvertNumMode": 1}
         self.create_rec_task_req._deserialize(params)
         self.create_rec_task_req.EngineModelType = '16k_zh_large'
+        #self.create_rec_task_req.EngineModelType = '16k_zh'
 
     def create_rec_task(self, file_url):
         logger.debug("Creating rec task")
@@ -53,7 +54,7 @@ class MyASRClient:
             try:
                 resp = self.client.DescribeTaskStatus(self.query_rec_task_req)
                 resp_json = resp.to_json_string()
-                logger.info(resp_json)
+                #logger.info(resp_json)
                 resp_obj = json.loads(resp_json)
                 if resp_obj["Data"]["StatusStr"] == "success":
                     result = resp_obj["Data"]["ResultDetail"]
